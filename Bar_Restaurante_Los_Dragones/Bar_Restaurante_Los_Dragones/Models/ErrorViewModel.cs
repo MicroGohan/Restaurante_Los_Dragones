27ccd7eb514e37aa6b70b1ef25f501ffa575bd1f
MicroGohan <wilsong273070@gmail.com>
2024-05-26 10:40:10 -0600

    creación del proyecto

---
+++ Bar_Restaurante_Los_Dragones/Bar_Restaurante_Los_Dragones/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bar_Restaurante_Los_Dragones.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
