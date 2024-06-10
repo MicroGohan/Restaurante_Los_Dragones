bcd3d0ace0b5070fc0c37c7c3305a3891bb5899e
KARLY PANIAGUA <kpaniagua70754@ufide.ac.cr>
2024-06-10 17:47:50 -0600

    Cambios del indentity

---
+++ Bar_Restaurante_Los_Dragones/Bar_Restaurante_Los_Dragones/Models/AuthContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bar_Restaurante_Los_Dragones.Models
+{
+    public class AuthContext : IdentityDbContext
+    {
+        public AuthContext() { }
+
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
+
--- Bar_Restaurante_Los_Dragones/Bar_Restaurante_Los_Dragones/Program.cs
+++ Bar_Restaurante_Los_Dragones/Bar_Restaurante_Los_Dragones/Program.cs
@@ -1,7 +1,36 @@
+
+using Bar_Restaurante_Los_Dragones.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("ConnDB") ?? throw new InvalidOperationException("Connection string 'ConnDB' not found.");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<LosDragonesDBContext>(options => options.UseSqlServer("name=ConnDB"));
+
+builder.Services.AddDbContext<AuthContext>(options => options.UseSqlServer("name=ConnDB"));
+
+
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AuthContext>().AddDefaultTokenProviders();
+
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Identity/Account/Login"; // Ruta de inicio de sesi�n
+    options.LogoutPath = "/Identity/Account/Logout"; // Ruta de cierre de sesi�n
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied"; // Ruta de acceso denegado
+});
+
+builder.Services.AddRazorPages();
+
 
 var app = builder.Build();
 
@@ -9,19 +38,24 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
+
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=home}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
