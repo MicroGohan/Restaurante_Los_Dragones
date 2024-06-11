23df869f02e7a34960faed6ec361c244ebcba124
KARLY PANIAGUA <kpaniagua70754@ufide.ac.cr>
2024-06-11 15:41:01 -0600

    Identity Funcionando

---
+++ Bar_Restaurante_Los_Dragones/Bar_Restaurante_Los_Dragones/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Bar_Restaurante_Los_Dragones.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
